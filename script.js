ccbbbd6054211185fc6cdea63df5d87b587443dd
MrSmexyPants <38173816+MrSmexyPants@users.noreply.github.com>
2024-07-22 12:56:03 +0200

    Create script.js
    add script for form page

---
+++ script.js
@@ -0,0 +1,29 @@
+
+        document.addEventListener('DOMContentLoaded', function() {
+          //References to each element in the form
+          const radioButtons = document.querySelectorAll('input[name="product"]');
+          const quantityInput = document.getElementById('amount');
+          const calculateTotal = document.getElementById('submit');
+          const resultElement = document.getElementById('total');
+
+          //Function to calcluate the results
+          function calculateResult() {
+            //get the selected radiobutton amount
+            const selectedRadio = document.querySelector('input[name="product"]:checked'];
+            if (!selectedRadio) {
+              alert("Please select a radio button value.");
+              return;
+            }
+
+            const radioValue = parseFloat(selectedRAdio.value);
+            const quantity = parseFloat(qunatityInput.value);
+
+            //Calculate results
+            const result = radioValue * quantity;
+
+            //Display results
+            resultElement.textContent = result;
+          }
+
+          calculateButton.addEventListener('click', calculateResult);
+        });
